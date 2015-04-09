330317b210b532dbab234f2725f045fcff347e84
marcandregirard <marcandr.girard@gmail.com>
2015-04-08 20:16:14 -0400

    Add statics pages to the repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root 'static_pages#home'
+  get 'static_pages/home'
+  get 'static_pages/help'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
