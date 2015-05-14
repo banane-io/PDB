4f4c8f1f26daa55779adfd5c579af598c787d9c2
Marc-Andre <marcandr.girard@gmail.com>
2015-05-14 19:45:15 -0400

    started the on click thing

--- app/controllers/grid_controller.rb
+++ app/controllers/grid_controller.rb
@@ -1,6 +1,7 @@
 class GridController < ApplicationController
   def show
     @map_points = MapPoint.all.group_by(&:x)
+    gon.map_points = Hash[*MapPoint.all.map{ |p| [p.id, p] }.flatten]
   end
 
   def move
