cd6ffbc90e3bb177807ce4b2c3caa4d0b44c60df
Marc-Andre <marcandr.girard@gmail.com>
2015-05-18 14:13:06 -0400

    fix entities

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -69,6 +69,6 @@ class EntitiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def entity_params
-      params[:entity]
+      params.require(:entity).permit(:name, :description, :map_point_id)
     end
 end
