3f2c628707d025f034453baeb253d29b03e54eab
Marc-Andre <marcandr.girard@gmail.com>
2015-06-01 19:51:15 -0400

    removed notice in entities

--- app/controllers/grid_controller.rb
+++ app/controllers/grid_controller.rb
@@ -1,8 +1,8 @@
 class GridController < ApplicationController
   def show
-    @map_points = MapPoint.all.group_by(&:x)
+    @map_points = MapPoint.includes(:entities).all.group_by(&:x)
     gon.map_points = Hash[*MapPoint.all.map{ |p| [p.id, p] }.flatten]
-    
+
   end
 
   def move
