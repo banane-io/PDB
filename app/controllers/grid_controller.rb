c99f0d3b3b4b153635513edf74c92ddf5d8b8591
Marc-Andre <marcandr.girard@gmail.com>
2015-06-03 21:20:07 -0400

    Done with the entity thing in grid#show

--- app/controllers/grid_controller.rb
+++ app/controllers/grid_controller.rb
@@ -1,13 +1,21 @@
 class GridController < ApplicationController
+  before_action :set_map_point, only: [:point]
   def show
     @map_points = MapPoint.includes(:entities).all.group_by(&:x)
-    @map_point = MapPoint.includes(:entities).first
+    @map_point = MapPoint.includes(:entities).first #I need to remove this
   end
 
   def point
-
+    render partial: 'map_points/map_point'
   end
 
   def move
   end
+
+  private
+
+    def set_map_point
+      @map_point = MapPoint.includes(:entities).find(params[:id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'grid/show'
+  get 'grid/point/:id', to: 'grid#point'
   get 'grid/move'
   resources :map_points
   resources :entities
