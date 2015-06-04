51369b18c53240ccbb7f00dd2fcb88d28fb88ce0
Marc-Andre <marcandr.girard@gmail.com>
2015-06-03 21:42:44 -0400

    Add entities to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 i = 0
 5.times do |x|
   5.times do |y|
-    MapPoint.create!(x: x, y: y, zone: "Zone#{i}")
+    map_point = MapPoint.create(x: x, y: y, zone: "Zone#{i}")
+    map_point.entities.build(name: "Test#{i}", description: "This is a simple description", map_point_id: map_point)
+    map_point.save
     i += 1
   end
 end
