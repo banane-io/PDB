d10dbc88efaecc71c68113e79411854384988b82
Marc-Andre <marcandr.girard@gmail.com>
2015-08-18 09:51:09 -0400

    Function to get neighbors tile

---
+++ app/classes/directions.rb
@@ -0,0 +1,22 @@
+class Directions
+  attr_accessor(:dx, :dy)
+  def initialize(dx, dy)
+    @dx = dx
+    @dy = dy
+  end
+
+  NORTHWEST = new(-1, -1)
+  NORTH = new(0, -1)
+  NORTHEAST = new(1, -1)
+  EAST = new(1, 0)
+  SOUTHEAST = new(1, 1)
+  SOUTH = new(0, -1)
+  SOUTHWEST = new(-1, 1)
+  WEST = new(-1, 0)
+
+  DIRECTIONS = [ NORTHWEST, NORTH, NORTHEAST, EAST, SOUTHEAST, SOUTH, SOUTHWEST,
+      WEST ]
+  class << self
+    private :new
+  end
+end
--- app/models/map_point.rb
+++ app/models/map_point.rb
@@ -2,4 +2,11 @@ class MapPoint < ActiveRecord::Base
   has_many :entities
   validates :x, presence: true
   validates :y, presence: true
+
+  def neighbors
+    neighbors = {}
+    Directions::DIRECTIONS.each{ |direction|
+      neighbors[direction] = MapPoint.find_by(x: direction.dx + self.x, y: direction.dy + self.y)
+    }
+  end
 end
