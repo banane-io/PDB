e426f18f8346a510d153794b8b1b8432a9bbee89
Marc-Andre <marcandr.girard@gmail.com>
2015-08-30 13:14:56 -0400

    Redirect to player creation after sign in if you don't have a player

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(resource_or_scope)
+    if current_user.player.nil?
+       new_player_path
+    else
+       root_path
+    end
+end
+
   include ApplicationHelper
 end
