054502671ad34dc1544925ccf2d38e5ebb6907d8
Thomas Lepage <thomas.lepage@hotmail.ca>
2015-09-24 18:22:26 -0400

    Fix the seed to generate terrain

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,23 @@
-terrain = Terrain.create(name: "test", colour: "green")
-terrain.save
+plain = Terrain.create(name: "plain", colour: "green")
+swamp = Terrain.create(name: "swamp", colour: "DarkGreen")
+beach = Terrain.create(name: "beach", colour: "yellow")
+mountain = Terrain.create(name: "mountain", colour: "gray")
+
+plain.save
+swamp.save
+beach.save
+mountain.save
+
+terrains = Array.new
+terrains.push plain
+terrains.push swamp
+terrains.push beach
+terrains.push mountain
 
 i = 0
 5.times do |x|
   5.times do |y|
-    map_point = MapPoint.create(x: x, y: y, zone: "Zone#{i}", terrain_id: terrain.id)
+    map_point = MapPoint.create(x: x, y: y, zone: "Zone#{i}", terrain_id: terrains[rand(terrains.length)].id)
     map_point.entities.build(name: "Test#{i}", description: "This is a simple description", map_point_id: map_point)
     map_point.save
     i += 1
