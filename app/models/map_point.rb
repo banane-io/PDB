373e56cb229a07e9e5aa67665c728aee2edf4847
Marc-Andre <marcandr.girard@gmail.com>
2015-09-28 21:41:43 -0400

    Fix #40

--- app/models/map_point.rb
+++ app/models/map_point.rb
@@ -21,17 +21,21 @@ class MapPoint < ActiveRecord::Base
 
   private
     def calculateMinMax(range_map, minMap, maxMap, position)
+      puts "minMap " + minMap.to_s
+      puts "maxMap " + maxMap.to_s
       overflow = 0
       underflow = 0
       borderLeft = position - range_map
+      puts "borderleft " + borderLeft.to_s
       borderRight = position + range_map
+      puts "borderRight " + borderRight.to_s
       if(borderLeft < minMap)
         overflow = borderLeft.abs
       end
       if(borderRight > maxMap)
-        underflow = maxMap - borderRight
+        underflow = borderRight - maxMap
       end
-
+      puts "underflow " + underflow.to_s
       min = [minMap, borderLeft].max
       min = min - underflow
       max = [maxMap, borderRight].min
