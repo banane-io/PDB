c548b1be53fbd95b82896649b15f56ea2734df85
Thomas Lepage <thomas.lepage@hotmail.ca>
2015-10-02 19:56:22 -0400

    Finish the authorization in the views. Change the redirect page to the custom unauthorize page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do | exception |
-    redirect_to root_url, alert: exception.message
+    redirect_to :unauthorized_redirect_path, alert: exception.message
   end
 
   def after_sign_in_path_for(resource_or_scope)
