bf4413df31c66f3802ddf835dc5eb9992ba0fc3b
Marc-Andre <marcandr.girard@gmail.com>
2015-10-09 15:26:56 -0400

    Add test for the neighbors method

--- test/models/map_point_test.rb
+++ test/models/map_point_test.rb
@@ -18,4 +18,38 @@ class MapPointTest < ActiveSupport::TestCase
     @map_point.y = nil
     assert_not @map_point.valid?
   end
+
+  test "complete grid of neighbors" do
+    neighbors = map_points(:five).neighbors
+    assert_equal 8, neighbors.length
+    Directions::DIRECTIONS.each{ |direction|
+      assert_not neighbors[direction].nil?
+    }
+  end
+
+  test "imcomplete grid of neighbors" do
+    neighbors = map_points(:one).neighbors
+    assert_equal 8, neighbors.length
+    assert neighbors[Directions::NORTHWEST].nil?
+    assert neighbors[Directions::NORTH].nil?
+    assert neighbors[Directions::NORTHEAST].nil?
+    assert_not neighbors[Directions::EAST].nil?
+    assert neighbors[Directions::WEST].nil?
+    assert neighbors[Directions::SOUTHWEST].nil?
+    assert_not neighbors[Directions::SOUTH].nil?
+    assert_not neighbors[Directions::SOUTHEAST].nil?
+  end
+
+  test "none neighbors" do
+    neighbors = @map_point.neighbors
+    assert_equal 8, neighbors.length
+    assert neighbors[Directions::NORTHWEST].nil?
+    assert neighbors[Directions::NORTH].nil?
+    assert neighbors[Directions::NORTHEAST].nil?
+    assert neighbors[Directions::EAST].nil?
+    assert neighbors[Directions::WEST].nil?
+    assert neighbors[Directions::SOUTHWEST].nil?
+    assert neighbors[Directions::SOUTH].nil?
+    assert neighbors[Directions::SOUTHEAST].nil?
+  end
 end
