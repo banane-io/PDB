769e3fe6e2a983724cf0988dbf1ceec1f40e4d41
Marc-Andre <marcandr.girard@gmail.com>
2015-10-09 16:06:52 -0400

    Removed out-of-date tests

--- test/controllers/grid_controller_test.rb
+++ test/controllers/grid_controller_test.rb
@@ -1,14 +1,7 @@
 require 'test_helper'
 
 class GridControllerTest < ActionController::TestCase
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
 
-  test "should get move" do
-    get :move
-    assert_response :success
-  end
+
 
 end
