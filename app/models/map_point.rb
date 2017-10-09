6715580101c9c7e735fa5dbe0e2ac3d909baaabf
Marc-Andre Girard <marcandr.girard@gmail.com>
2017-10-08 21:41:09 -0400

    reformat per Phrancis small review in chat, thanks!

--- app/models/map_point.rb
+++ app/models/map_point.rb
@@ -14,26 +14,24 @@ class MapPoint < ActiveRecord::Base
 
   def load_grid(range)
     range_map = range / 2
-    minMaxX = calculateMinMax(range_map, MapPoint.minimum("x"), MapPoint.maximum("x"), self.x)
-    minMaxY = calculateMinMax(range_map, MapPoint.minimum("y"), MapPoint.maximum("y"), self.y)
+    minMaxX = calculate_Min_Max(range_map, MapPoint.minimum("x"), MapPoint.maximum("x"), self.x)
+    minMaxY = calculate_Min_Max(range_map, MapPoint.minimum("y"), MapPoint.maximum("y"), self.y)
     MapPoint.includes(:entities).where(:x => minMaxX[0]..minMaxX[1]).where(:y => minMaxY[0]..minMaxY[1]).order(x: :asc, y: :asc).group_by(&:y)
   end
 
   private
-    def calculateMinMax(range_map, minMap, maxMap, position)
+    def calculate_Min_Max(range_map, min_map, max_map, position)
       overflow = 0
       underflow = 0
       borderLeft = position - range_map
       borderRight = position + range_map
-      if(borderLeft < minMap)
-        overflow = borderLeft.abs
-      end
-      if(borderRight > maxMap)
-        underflow = borderRight - maxMap
-      end
-      min = [minMap, borderLeft].max
+
+      overflow = borderLeft.abs if borderLeft < _
+      underflow = borderRight - max_map if borderRight > max_map
+      
+      min = [_, borderLeft].max
       min = min - underflow
-      max = [maxMap, borderRight].min
+      max = [max_map, borderRight].min
       max = max + overflow
       return [min,max]
     end
