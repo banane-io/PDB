3044951064a9a985bd9d949c3f1c67eb2a761397
Marc-Andre Girard <marcandr.girard@gmail.com>
2019-03-03 15:30:51 -0500

    Add the api prefix to all the rest controller

--- src/main/java/banane/io/pdb/web/GridController.java
+++ src/main/java/banane/io/pdb/web/GridController.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/grid")
+@RequestMapping("/grid")
 public class GridController {
 
     @Autowired
