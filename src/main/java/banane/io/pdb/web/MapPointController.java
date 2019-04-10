04d9d07df519a7043ff5cb74e252b295f6745507
Marc-Andre Girard <marcandr.girard@gmail.com>
2019-04-09 21:15:17 -0400

    First draft of processing an action done by the client
    This is a basic basic implementation of processing actions sent from the client.

--- src/main/java/banane/io/pdb/web/MapPointController.java
+++ src/main/java/banane/io/pdb/web/MapPointController.java
@@ -2,18 +2,28 @@ package banane.io.pdb.web;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
+import com.google.common.base.Strings;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.BasicJsonParser;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import banane.io.pdb.model.MapPoint;
 import banane.io.pdb.model.Terrain;
 import banane.io.pdb.repository.MapPointRepository;
+import banane.io.pdb.security.SecurityService;
 
 @RestController
 @RequestMapping("/api/mapPoint")
@@ -21,6 +31,9 @@ public class MapPointController {
 
     @Autowired
     private MapPointRepository mapPointRepository;
+
+    @Autowired
+    private SecurityService securityService;
 
     private static final Logger logger = LoggerFactory.getLogger(MapPointController.class);
 
@@ -39,15 +52,31 @@ public class MapPointController {
         logger.info("Fetched mapPoint: {} with x: {} and y: {}", zoneId.toString(), mapPoint.getX(), mapPoint.getY());
         Terrain terrain = mapPoint.getTerrain();
         List<String> actions = new LinkedList<>();
-        if(Terrain.MOUNTAIN.equals(terrain)) {
+        if (Terrain.MOUNTAIN.equals(terrain)) {
             actions.add("MINE");
-        } else if (Terrain.FOREST.equals(terrain) ){
+        } else if (Terrain.FOREST.equals(terrain)) {
             actions.add("LOGGING");
             actions.add("MINE");
         }
-        
+
         return actions;
     }
-    
 
+    @PostMapping("/{id}/action")
+    public ResponseEntity<?> doAction(@PathVariable("id") Long zoneId, @RequestBody String action) {
+        logger.info("Processing the action for the zone {}", zoneId);
+        if (Strings.isNullOrEmpty(action)) {
+            logger.warn("Action was null or empty, {} sent a BAD_REQUEST", securityService.findLoggedInUsername());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error on the action");
+        }
+        JsonParser jsonParser = new BasicJsonParser();
+        Map<String, Object> parseMap = jsonParser.parseMap(action);
+        String object = (String) parseMap.get("action");
+        if (object.equalsIgnoreCase("MINE")) {
+            logger.info("Procressing MINE ACTION for zone: {} player : {}", zoneId, securityService.findLoggedInUsername());
+        } else if (object.equalsIgnoreCase("LOGGING")) {
+            logger.info("Procressing LOGGING ACTION for zone: {} player : {}", zoneId, securityService.findLoggedInUsername());
+        }
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("");
+    }
 }
