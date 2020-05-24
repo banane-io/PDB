5f7f56ef94df65ae74939df069094c7b79d9d1e4
Marc-Andre Girard <marcandr.girard@gmail.com>
2020-05-24 12:18:49 -0400

    Added test with mock. Still not perfect tests but it's getting better

--- src/test/banane/io/pdb/service/ActionServiceImplTest.kt
+++ src/test/banane/io/pdb/service/ActionServiceImplTest.kt
@@ -3,14 +3,25 @@ package banane.io.pdb.service
 import banane.io.pdb.model.Action
 import banane.io.pdb.model.MapPoint
 import banane.io.pdb.model.Terrain
+import banane.io.pdb.model.User
+import banane.io.pdb.repository.BaseRepository
+import banane.io.pdb.repository.HeroRepository
+import banane.io.pdb.security.SecurityService
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Test
 import kotlin.test.assertEquals
 
 internal class ActionServiceImplTest {
-    private val serviceToTest = ActionServiceImpl()
 
     @Test
     fun `Verify that Mountains return Mine action`() {
+        val securityService = mockk<SecurityService>()
+        val heroRepository = mockk<HeroRepository>()
+        val baseRepository = mockk<BaseRepository>()
+
+        val serviceToTest = ActionServiceImpl(securityService, heroRepository, baseRepository)
+
         val mountainPoint = MapPoint()
         mountainPoint.id = 1;
         mountainPoint.terrain = Terrain.MOUNTAIN
@@ -23,7 +34,12 @@ internal class ActionServiceImplTest {
     }
 
     @Test
-    fun `Verify that Forest return both Logging and Minning actions`() {
+    fun `Verify that Forest return both Logging and Miening actions`() {
+        val securityService = mockk<SecurityService>()
+        val heroRepository = mockk<HeroRepository>()
+        val baseRepository = mockk<BaseRepository>()
+
+        val serviceToTest = ActionServiceImpl(securityService, heroRepository, baseRepository)
         val forestMapPoint = MapPoint()
         forestMapPoint.id = 1;
         forestMapPoint.terrain = Terrain.FOREST
@@ -34,5 +50,25 @@ internal class ActionServiceImplTest {
         assertEquals(2, availableActionsFromMapPoint.size, "There's should only be two actions availables")
         assert(availableActionsFromMapPoint.contains(Action.MINE))
         assert(availableActionsFromMapPoint.contains(Action.LOGGING))
+    }
+
+    @Test
+    fun `No action on plains when User is null`() {
+        val securityService = mockk<SecurityService>()
+        every { securityService.findLoggedInUser()} returns null
+        val heroRepository = mockk<HeroRepository>()
+        val baseRepository = mockk<BaseRepository>()
+
+        val serviceToTest = ActionServiceImpl(securityService, heroRepository, baseRepository)
+
+        val plainMapPoint = MapPoint()
+        plainMapPoint.id = 1;
+        plainMapPoint.terrain = Terrain.PLAIN
+        plainMapPoint.x = 1
+        plainMapPoint.y = 1
+        plainMapPoint.zone = "This is a description"
+
+        val availableActionsFromMapPoint = serviceToTest.getAvailablesActionsFromMapPoint(plainMapPoint)
+        assert(availableActionsFromMapPoint.isEmpty())
     }
 }
