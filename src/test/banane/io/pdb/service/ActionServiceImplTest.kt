67a43f8d4a34b98e976e6a5755621c30d1e593fa
Marc-Andre Girard <marcandr.girard@gmail.com>
2020-05-23 22:22:49 -0400

    Added a test and fix typo in previous method name

--- src/test/banane/io/pdb/service/ActionServiceImplTest.kt
+++ src/test/banane/io/pdb/service/ActionServiceImplTest.kt
@@ -4,12 +4,13 @@ import banane.io.pdb.model.Action
 import banane.io.pdb.model.MapPoint
 import banane.io.pdb.model.Terrain
 import org.junit.Test
+import kotlin.test.assertEquals
 
 internal class ActionServiceImplTest {
     private val serviceToTest = ActionServiceImpl()
 
     @Test
-    fun `Verify that Mountains return Mine cation`() {
+    fun `Verify that Mountains return Mine action`() {
         val mountainPoint = MapPoint()
         mountainPoint.id = 1;
         mountainPoint.terrain = Terrain.MOUNTAIN
@@ -17,6 +18,21 @@ internal class ActionServiceImplTest {
         mountainPoint.y = 1
         mountainPoint.zone = "This is a description"
         val availableActionsFromMapPoint = serviceToTest.getAvailablesActionsFromMapPoint(mountainPoint)
+        assertEquals(1, availableActionsFromMapPoint.size, "There's should only be one action available")
         assert(availableActionsFromMapPoint.contains(Action.MINE))
+    }
+
+    @Test
+    fun `Verify that Forest return both Logging and Minning actions`() {
+        val forestMapPoint = MapPoint()
+        forestMapPoint.id = 1;
+        forestMapPoint.terrain = Terrain.FOREST
+        forestMapPoint.x = 1
+        forestMapPoint.y = 1
+        forestMapPoint.zone = "This is a description"
+        val availableActionsFromMapPoint = serviceToTest.getAvailablesActionsFromMapPoint(forestMapPoint)
+        assertEquals(2, availableActionsFromMapPoint.size, "There's should only be two actions availables")
+        assert(availableActionsFromMapPoint.contains(Action.MINE))
+        assert(availableActionsFromMapPoint.contains(Action.LOGGING))
     }
 }
