c6b100ee2c33249f78c2ecc314807ce1d701d02a
marcandregirard <marcandr.girard@gmail.com>
2022-03-07 11:25:17 -0500

    Added postgres connection in the setup

---
+++ ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PDB
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using PDB;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var builderConfiguration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+IConfiguration config = builderConfiguration.Build();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connectionString = config["PostgreSql:ConnectionString"];
+var dbPassword = config["PostgreSql:DbPassword"];
+var builderDbConnection = new NpgsqlConnectionStringBuilder(connectionString)
+{
+    Password = dbPassword
+};
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(builderDbConnection.ConnectionString));
 
 var app = builder.Build();
 
