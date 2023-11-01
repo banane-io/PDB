322d2757d057a3cba5912b155623288c2d6ccde6
marcandregirard <marcandr.girard@gmail.com>
2023-11-01 10:09:34 -0400

    Upgrading runtime to .net7

--- Services/MapPointService.cs
+++ Services/MapPointService.cs
@@ -6,7 +6,6 @@ namespace PDB.Services
     public class MapPointService : IMapPointService
     {
         private readonly ApplicationContext _context;
-
         //Those are constant for that map that will be variable at some point.
         private const int XMinimumValue = 0;
         private const int YMinimumValue = 0;
@@ -28,16 +27,15 @@ namespace PDB.Services
 
         public async Task<IEnumerable<IEnumerable<MapPoint>>> LoadGrid(MapPoint center)
         {
-            (int x1, int x2) = calculateMinMax(GridRange, XMinimumValue, XMaximumValue, center.X);
-            (int y1, int y2) = calculateMinMax(GridRange, YMinimumValue, YMaximumValue, center.Y);
+            (int x1, int x2) = CalculateMinMax(GridRange, XMinimumValue, XMaximumValue, center.X);
+            (int y1, int y2) = CalculateMinMax(GridRange, YMinimumValue, YMaximumValue, center.Y);
 
             var grid = await _context.MapPoints.Where(x => x.X >= x1 && x.X <= x2 && x.Y >= y1 && x.Y <= y2).OrderBy(x => x.X).ThenBy(x => x.Y).ToListAsync();
             return grid.GroupBy(x => x.X);
         }
 
-        private (int, int) calculateMinMax(int radius, int minMap, int maxMap, int center)
+        private (int, int) CalculateMinMax(int radius, int minMap, int maxMap, int center)
         {
-
             var overflow = 0;
             var underflow = 0;
             var borderLeft = center - radius;
